365d82a3addd8bb7e7cefc5ce2883e518ec243dc
fchabanois <flocha2000-agile@yahoo.fr>
2011-07-24 23:11:29 +0200

    formatage et adaptation du main

--- main/main_example.sh
+++ main/main_example.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 SEUIL_ERREURS_TOTAL=10
 SEUIL_ERREURS_DISTINCTES=2
 
@@ -12,26 +11,23 @@ LOG_FILE=./sampleKeljob.log
 date
 
 echo ""
-getNbErreurs $LOG_FILE
-nbErreursTotal=$?
+nbErreursTotal=`getNbErreurs $LOG_FILE`
 echo "Nombre d'erreurs au total : $nbErreursTotal"
 
 echo ""
-getNbErreursDistinctes $LOG_FILE
-nbErreursDistinctes=$?
-
+nbErreursDistinctes=`getNbErreursDistinctes $LOG_FILE`
 echo ""
 echo "Nombre d'erreurs distinctes : $nbErreursDistinctes"
 echo ""
 
 if [ $nbErreursTotal -gt $SEUIL_ERREURS_TOTAL ]; then
-	afficheErreur "[FAILED] Le nombre d'erreurs totales a dépassé le seuil de $SEUIL_ERREURS_TOTAL" 
-	exit 1
+  afficheErreur "[FAILED] Le nombre d'erreurs totales a dépassé le seuil de $SEUIL_ERREURS_TOTAL" 
+  exit 1
 fi
 
 if [ $nbErreursDistinctes -gt $SEUIL_ERREURS_DISTINCTES ]; then
   afficheErreur "[FAILED] Le nombre d'erreurs distinctes a dépassé le seuil de $SEUIL_ERREURS_DISTINCTES" 
-	exit 2
+  exit 2
 fi
 
 
--- main/verifie_logs.sh
+++ main/verifie_logs.sh
@@ -1,41 +1,40 @@
 #/bin/bash
 
 getNbErreurs(){
-	logFile=$1
-	grep -wc "ERROR" ${logFile}
+  logFile=$1
+  grep -wc "ERROR" ${logFile}
 }
 
 getNbErreursDistinctes(){
- 	logFile=$1
-	tempLog='/tmp/verifie_logs.log'
-	grep -w "ERROR" ${logFile} | awk -F] '{print $2}' > ${tempLog}
-	rendIdAnonyme ${tempLog} > ${tempLog}_sans_id
-	aplatitDateJusqueProchainEspace ${tempLog}_sans_id > ${tempLog}_sans_id_ni_date
+  logFile=$1
+  tempLog='/tmp/verifie_logs.log'
+  grep -w "ERROR" ${logFile} | awk -F] '{print $2}' > ${tempLog}
+  rendIdAnonyme ${tempLog} > ${tempLog}_sans_id
+  aplatitDateJusqueProchainEspace ${tempLog}_sans_id > ${tempLog}_sans_id_ni_date
   aplatitDocumentEtParent ${tempLog}_sans_id_ni_date > ${tempLog}_sans_id_ni_date_ni_docs
-	rendEmailAnonyme ${tempLog}_sans_id_ni_date_ni_docs | sort | uniq -c > ${tempLog}_resume
-	nbErreurs=`cat ${tempLog}_resume | wc -l`
-	echo "$nbErreurs"
-	sort -nr ${tempLog}_resume >&2
-	rm -f $tempLog*
+  rendEmailAnonyme ${tempLog}_sans_id_ni_date_ni_docs | sort | uniq -c > ${tempLog}_resume
+  nbErreurs=`cat ${tempLog}_resume | wc -l`
+  echo "$nbErreurs"
+  sort -nr ${tempLog}_resume >&2
+  rm -f $tempLog*
 }
 
 rendEmailAnonyme(){
-	logFile=$1
-	sed 's/[.a-zA-Z0-9\_\-]*@[.a-zA-Z0-9\-\-]*/mailXXX/g' $logFile 
+  logFile=$1
+  sed 's/[.a-zA-Z0-9\_\-]*@[.a-zA-Z0-9\-\-]*/mailXXX/g' $logFile 
 }
 
 rendIdAnonyme(){
-	logFile=$1
-	sed 's/id=[.a-zA-Z0-9\_\-]*/id=XXX/g' $logFile 
+  logFile=$1
+  sed 's/id=[.a-zA-Z0-9\_\-]*/id=XXX/g' $logFile 
 }
 
 aplatitDateJusqueProchainEspace() {
-	logFile=$1
-	sed 's#[0-9][0-9]/[0-9][0-9]/\([0-9]\)\{4\} [0-9][0-9]:[0-9][0-9][.:a-zA-Z0-9\_\-]*#XX/XX/XXXX XX:XX:XX#g' $logFile 
+  logFile=$1
+  sed 's#[0-9][0-9]/[0-9][0-9]/\([0-9]\)\{4\} [0-9][0-9]:[0-9][0-9][.:a-zA-Z0-9\_\-]*#XX/XX/XXXX XX:XX:XX#g' $logFile 
 }
 
 aplatitDocumentEtParent(){
-	logFile=$1
-	sed 's#\.pdf#\.doc#gi' $logFile | sed 's#/[[:alnum:]\_]*/[[:alnum:]\_]*\.docx\{0,1\}#/XXX/document#gi'
-
+  logFile=$1
+  sed 's#\.pdf#\.doc#gi' $logFile | sed 's#/[[:alnum:]\_]*/[[:alnum:]\_]*\.docx\{0,1\}#/XXX/document#gi'
 }
