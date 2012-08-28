30eea852318e434ddcded30656539d09014cf656
fchabanois <flocha2000-agile@yahoo.fr>
2012-08-28 18:47:22 +0200

    sample properties file updated

--- main/propertiesSample.sh
+++ main/propertiesSample.sh
@@ -8,3 +8,9 @@ export SERVEUR_USER=$NOM_APPLICATION
 export FILE_TO_GET=/var/log/apps/keljob/keljob.log
 export LOG_FILE=/tmp/${NOM_APPLICATION}-webapp.log
 export CONVERT_LOG_TO_UTF8=no|yes
+export COMPRESS_IF_TOO_BIG_MO=500 #if empty, it won't be compressed before remote copy #export COMPRESS_IF_TOO_BIG_MO=""
+
+#specifique pour apache
+export APACHE_FILE_TO_GET=/var/log/apache2/www.mywebsite.com_access.log.1				#it can be a zipped file, as long as zgrep can parse it
+export APACHE_LOG_FILE=~/tmp/${NOM_APPLICATION}-apache.log                      #dans /home car /tmp est trop petit. Le fichier fait 1 GO par frontal.
+
