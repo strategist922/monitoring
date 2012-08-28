8f0e21e2d29482bbcb611dd4f0210030da847f91
fchabanois <flocha2000-agile@yahoo.fr>
2012-08-28 18:52:19 +0200

    sample properties file updated

--- main/propertiesSample.sh
+++ main/propertiesSample.sh
@@ -5,7 +5,7 @@ export SEUIL_ERREURS_DISTINCTES=15
 export SEUIL_ERREURS_TOTAL=280
 export SERVEURS="frontal1 frontal2 frontal3"
 export SERVEUR_USER=$NOM_APPLICATION
-export FILE_TO_GET=/var/log/apps/keljob/keljob.log
+export FILE_TO_GET=/var/log/apps/mywebsite/webapp.log
 export LOG_FILE=/tmp/${NOM_APPLICATION}-webapp.log
 export CONVERT_LOG_TO_UTF8=no|yes
 export COMPRESS_IF_TOO_BIG_MO=500 #if empty, it won't be compressed before remote copy #export COMPRESS_IF_TOO_BIG_MO=""
