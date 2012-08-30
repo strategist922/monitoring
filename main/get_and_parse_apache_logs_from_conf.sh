45aecb22705c3315dc30618ef11cad1257c9e101
fchabanois <flocha2000-agile@yahoo.fr>
2012-08-30 18:21:32 +0200

    refactor

--- main/get_and_parse_apache_logs_from_conf.sh
+++ main/get_and_parse_apache_logs_from_conf.sh
@@ -26,7 +26,9 @@ headerEnd
 countDistinctInternalServerError $APACHE_LOG_FILE
 
 TOTAL_SERVER_ERROR=`countTotalInternalServerError $APACHE_LOG_FILE`
+echo ""
 echo "[INFO] Il y a eu $TOTAL_SERVER_ERROR erreurs 500 ";
+echo ""
 if [ "$TOTAL_SERVER_ERROR" -gt "$APACHE_SEUIL_ERREURS_500" ]; then
   echo ""
   afficheErreur "[FAILED] Il y a eu $TOTAL_SERVER_ERROR erreurs 500 (> seuil de $APACHE_SEUIL_ERREURS_500) ";
@@ -40,6 +42,5 @@ if [ $? -eq 0 ]; then
   headerBody "Ecrit les données dans un fichier à part pour le plugin plot de Jenkins"
 	echo "YVALUE=${TOTAL_SERVER_ERROR}" > ../reports/erreurs_500_totales
 fi
-
 
 exit $CODE_RETOUR
